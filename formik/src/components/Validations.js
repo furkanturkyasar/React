47f61b17b4afa9b73b2785aa5015f6545d7c435b
Furkan Turkyasar <turkyasarfurkan@gmail.com>
2022-06-26 22:45:43 +0300

    formik done

---
+++ formik/src/components/Validations.js
@@ -0,0 +1,11 @@
+import { object, string, number, date, InferType, ref } from "yup";
+
+const Validations = object({
+  email: string().email().required(),
+  password: string().min(5).required(),
+  passwordConfirm: string()
+    .oneOf([ref("password")])
+    .required(),
+});
+
+export default Validations;

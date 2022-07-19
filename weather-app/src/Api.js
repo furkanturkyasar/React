08c048f9c5c5110fa188d2593879639dd7509a93
Furkan Turkyasar <turkyasarfurkan@gmail.com>
2022-07-19 17:52:08 +0300

    Weather app with autocomplete done

---
+++ weather-app/src/Api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "717aaba4e3mshd5635afba0a893ep135ed6jsn6286caa2f934",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "3b87bc48175ba100eacde0444a6d39d8";
